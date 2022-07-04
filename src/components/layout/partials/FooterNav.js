ed9cb7f0299521d1cdb5c8931d2e33247fa3679a
Koutelier <koutelier@protonmail.com>
2022-07-04 13:56:36 +0300

    New better looking website for Satergo

---
+++ src/components/layout/partials/FooterNav.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const FooterNav = ({
+  className,
+  ...props
+}) => {
+
+  const classes = classNames(
+    'footer-nav',
+    className
+  );
+
+  return (
+    <nav
+      {...props}
+      className={classes}
+    >
+      <ul className="list-reset">
+        <li>
+        <a href="https://t.me/SatergoWallet">Telegram</a>
+        </li>
+        <li>
+        <a href="https://github.com/Satergo/Satergo#readme">Informations</a>
+        </li>
+        <li>
+          <a href='https://github.com/Satergo'>Github</a>
+        </li>
+        <li>
+         <a href='https://github.com/Satergo/Satergo/projects/1?fullscreen=true'>To Do list</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default FooterNav;
