441923efa84e7417339f463932a16576f91c1275
satsen <95986745+satsen@users.noreply.github.com>
2022-07-09 06:56:12 +0100

    Finished modal, dependency updates

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,9 +5,16 @@ import Logo from './partials/Logo';
 import { Modal } from 'react-responsive-modal';
 import 'react-responsive-modal/styles.css';
 
-
-
-
+window.addEventListener("click", function (e) {
+	if (e.target.hasAttribute("data-copy")) {
+		var textarea = document.createElement("textarea");
+		textarea.value = e.target.getAttribute("data-copy");
+		document.body.appendChild(textarea);
+		textarea.select();
+		document.execCommand("copy");
+		document.body.removeChild(textarea);
+	}
+});
 
 const propTypes = {
 	navPosition: PropTypes.string,
@@ -78,25 +85,12 @@ const Header = ({
 		bottomOuterDivider && 'has-bottom-divider',
 		className
 	);
-	// Modal 
-const [open, setOpen] = useState(false);
 
-const onOpenModal = () => setOpen(true);
-const onCloseModal = () => setOpen(false);
-//copy listener 
-window.addEventListener("click", function (e) {
-	if (e.target.hasAttribute("data-copy")) {
-	console.log(e.target.getAttribute("data-copy"));
-		var textarea = document.createElement("textarea");
-		textarea.value = e.target.getAttribute("data-copy");
-		document.body.appendChild(textarea);
-		textarea.select();
-		document.execCommand("copy");
-		document.body.removeChild(textarea);
-		e.preventDefault();
-		this.alert("Copied successfully")
-	}
-});
+	// Modal
+	const [open, setOpen] = useState(false);
+
+	const onOpenModal = () => setOpen(true);
+	const onCloseModal = () => setOpen(false);
 
 	return (
 		<header
@@ -127,27 +121,24 @@ window.addEventListener("click", function (e) {
 									)}>
 								<div className="header-nav-inner">
 									<ul className="list-reset header-nav-right">
-									<button className="button button-primary button-wide-mobile button-sm" onClick={onOpenModal}>Donate</button>
-      <Modal open={open} onClose={onCloseModal} center>
-	  <div className="title">
-					<h1 className="text-color-primary">Donate</h1>
-				</div>
-				
-				<div>
-					<p>~ Ergo: 9gMnqf29LPxos2Lk5Lt6SkTmbWYL1d5QFHygbf6zRXDgL4KtAho</p><a href="https://explorer.ergoplatform.com/en/addresses/9gMnqf29LPxos2Lk5Lt6SkTmbWYL1d5QFHygbf6zRXDgL4KtAho"> Explorer </a>
-					<a href={() => false} data-copy="9gMnqf29LPxos2Lk5Lt6SkTmbWYL1d5QFHygbf6zRXDgL4KtAho">copy</a>
-				</div>
+										<button className="button button-primary button-wide-mobile button-sm" onClick={onOpenModal}>Donate</button>
+										<Modal open={open} onClose={onCloseModal} center>
+											<div className="title">
+												<h1 className="text-color-primary">Donate</h1>
+											</div>
 
-				<div>
-					<p>~ Monero: 82pTXa9pXyee7Ft81sGnK8c6XK3qKf2FzWbgUFrPJGdART4krBcsXkCKeBX5TQRxBr9ryhdPyHa5wGBr37Em2SSgP4u9MmH </p><a href={() => false} data-copy="82pTXa9pXyee7Ft81sGnK8c6XK3qKf2FzWbgUFrPJGdART4krBcsXkCKeBX5TQRxBr9ryhdPyHa5wGBr37Em2SSgP4u9MmH"> copy </a>
-				</div>
+											<div className="donation-row">
+												<div className="donation-address invert-color"><b>Ergo:</b> 9gMnqf29LPxos2Lk5Lt6SkTmbWYL1d5QFHygbf6zRXDgL4KtAho</div> <a className="button button-sm" href="https://explorer.ergoplatform.com/en/addresses/9gMnqf29LPxos2Lk5Lt6SkTmbWYL1d5QFHygbf6zRXDgL4KtAho">explorer</a> <button className="button button-sm" data-copy="9gMnqf29LPxos2Lk5Lt6SkTmbWYL1d5QFHygbf6zRXDgL4KtAho">copy</button>
+											</div>
 
-				<div>
-					<p>~ Bitcoin: bc1qcn833ty7fhp84ed22kvateckq08ffhp627q3us</p> <a href="https://mempool.space/address/bc1qcn833ty7fhp84ed22kvateckq08ffhp627q3us"> Explorer </a><a href={() => false} data-copy="bc1qcn833ty7fhp84ed22kvateckq08ffhp627q3us">copy</a>
+											<div className="donation-row">
+												<div className="donation-address invert-color"><b>Monero:</b> 82pTXa9pXyee7Ft81sGnK8c6XK3qKf2FzWbgUFrPJGdART4krBcsXkCKeBX5TQRxBr9ryhdPyHa5wGBr37Em2SSgP4u9MmH</div> <button className="button button-sm" data-copy="82pTXa9pXyee7Ft81sGnK8c6XK3qKf2FzWbgUFrPJGdART4krBcsXkCKeBX5TQRxBr9ryhdPyHa5wGBr37Em2SSgP4u9MmH">copy</button>
+											</div>
 
-				</div>
-
-      </Modal>
+											<div className="donation-row">
+												<div className="donation-address invert-color"><b>Bitcoin:</b> bc1qcn833ty7fhp84ed22kvateckq08ffhp627q3us</div> <a className="button button-sm" href="https://mempool.space/address/bc1qcn833ty7fhp84ed22kvateckq08ffhp627q3us">explorer</a> <button className="button button-sm" data-copy="bc1qcn833ty7fhp84ed22kvateckq08ffhp627q3us">copy</button>
+											</div>
+										</Modal>
 									</ul>
 								</div>
 							</nav>
